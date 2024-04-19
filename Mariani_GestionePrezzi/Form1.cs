2f4dc29c823160d1b58be05a224e8976264800f8
ludo <mariani.ludovico.studente@itispaleocapa.it>
2024-04-19 13:05:36 +0200

    Impostazione di Base

--- Mariani_GestionePrezzi/Form1.cs
+++ Mariani_GestionePrezzi/Form1.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace Mariani_GestionePrezzi
 {
@@ -15,6 +16,33 @@ namespace Mariani_GestionePrezzi
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            RecipeManager<string> recipeManager = new RecipeManager<string>();
+
+            // Caricamento delle ricette dal file, se presente
+            string filePath = "ricette.json";
+            recipeManager.LoadRecipesFromFile(filePath);
+
+            // Aggiunta di una nuova ricetta
+            Recipe<string> newRecipe = new Recipe<string>("Nuova Sasd");
+            newRecipe.AddIngredient("Ingrediente 1", "Quantità 1");
+            newRecipe.AddIngredient("Ingrediente 2", "Quantità 2");
+            recipeManager.AddRecipe(newRecipe);
+
+            // Rimozione di una ricetta
+            /*if (recipeManager.Recipes.Count > 0)
+            {
+                recipeManager.RemoveRecipe(recipeManager.Recipes[0]);
+            }*/
+
+            // Salvataggio delle ricette aggiornate su file
+            recipeManager.SaveRecipesToFile(filePath);
+
+            // Visualizzazione di tutte le ricette
+            recipeManager.DisplayAllRecipes();
         }
     }
 }
---
+++ Mariani_GestionePrezzi/RecipeManager.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using Newtonsoft.Json;
+using System.Windows.Forms;
+
+public class Ingredient<T>
+{
+    public string Name { get; set; }
+    public T Quantity { get; set; }
+}
+
+public class Recipe<T>
+{
+    public string Name { get; set; }
+    public List<Ingredient<T>> Ingredients { get; set; }
+
+    public Recipe(string name)
+    {
+        Name = name;
+        Ingredients = new List<Ingredient<T>>();
+    }
+
+    public void AddIngredient(string name, T quantity)
+    {
+        Ingredients.Add(new Ingredient<T> { Name = name, Quantity = quantity });
+    }
+
+    public override string ToString()
+    {
+        return Name;
+    }
+}
+
+public class RecipeManager<T>
+{
+    public List<Recipe<T>> Recipes { get; set; }
+
+    public RecipeManager()
+    {
+        Recipes = new List<Recipe<T>>();
+    }
+
+    public void AddRecipe(Recipe<T> recipe)
+    {
+        Recipes.Add(recipe);
+    }
+
+    public void RemoveRecipe(Recipe<T> recipe)
+    {
+        Recipes.Remove(recipe);
+    }
+
+    public void SaveRecipesToFile(string filePath)
+    {
+        string json = JsonConvert.SerializeObject(Recipes, Formatting.Indented);
+        File.WriteAllText(filePath, json);
+        MessageBox.Show($"\nRicette salvate su '{filePath}'.");
+    }
+
+    public void LoadRecipesFromFile(string filePath)
+    {
+        if (File.Exists(filePath))
+        {
+            string json = File.ReadAllText(filePath);
+            Recipes = JsonConvert.DeserializeObject<List<Recipe<T>>>(json);
+            MessageBox.Show($"\nRicette caricate da '{filePath}'.");
+        }
+        else
+        {
+            MessageBox.Show($"\nIl file '{filePath}' non esiste. Nessuna ricetta caricata.");
+            Recipes = new List<Recipe<T>>(); // Imposta la lista di ricette su vuota
+        }
+    }
+
+    public void DisplayAllRecipes()
+    {
+        MessageBox.Show("Elenco delle ricette:");
+        foreach (var recipe in Recipes)
+        {
+            MessageBox.Show(recipe.Name);
+            foreach (var ingredient in recipe.Ingredients)
+            {
+                MessageBox.Show($"- {ingredient.Name}: {ingredient.Quantity}");
+            }
+            MessageBox.Show("");
+        }
+    }
+}
