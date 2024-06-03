7f0f55278d4b2b257b9559e6e34cb8f913bf6c1b
ludddo <mariani.ludovico.studente@itispaleocapa.it>
2024-06-03 18:48:52 +0200

    commit probabilmente finale (implementato il sito HTML)

--- Mariani_GestionePrezzi/FormVisualizzatoreMenu.Designer.cs
+++ Mariani_GestionePrezzi/FormVisualizzatoreMenu.Designer.cs
@@ -29,9 +29,8 @@
         private void InitializeComponent()
         {
             this.label5 = new System.Windows.Forms.Label();
-            this.dataGridViewProdotti = new System.Windows.Forms.DataGridView();
             this.label6 = new System.Windows.Forms.Label();
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProdotti)).BeginInit();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // label5
@@ -46,15 +45,6 @@
             this.label5.TabIndex = 12;
             this.label5.Text = "Brew Bucks";
             // 
-            // dataGridViewProdotti
-            // 
-            this.dataGridViewProdotti.BackgroundColor = System.Drawing.SystemColors.Control;
-            this.dataGridViewProdotti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridViewProdotti.Location = new System.Drawing.Point(145, 150);
-            this.dataGridViewProdotti.Name = "dataGridViewProdotti";
-            this.dataGridViewProdotti.Size = new System.Drawing.Size(279, 143);
-            this.dataGridViewProdotti.TabIndex = 13;
-            // 
             // label6
             // 
             this.label6.AutoSize = true;
@@ -66,19 +56,29 @@
             this.label6.TabIndex = 14;
             this.label6.Text = "Visualizzazione Menu";
             // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(210, 189);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 60);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Apri il Menu";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
             // FormVisualizzatoreMenu
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(40)))), ((int)(((byte)(49)))));
             this.ClientSize = new System.Drawing.Size(560, 361);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.label6);
-            this.Controls.Add(this.dataGridViewProdotti);
             this.Controls.Add(this.label5);
             this.Name = "FormVisualizzatoreMenu";
             this.Text = "FormVisualizzatoreMenu";
             this.Load += new System.EventHandler(this.FormVisualizzatoreMenu_Load);
-            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewProdotti)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -87,7 +87,7 @@
         #endregion
 
         private System.Windows.Forms.Label label5;
-        private System.Windows.Forms.DataGridView dataGridViewProdotti;
         private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button1;
     }
 }
--- Mariani_GestionePrezzi/FormVisualizzatoreMenu.cs
+++ Mariani_GestionePrezzi/FormVisualizzatoreMenu.cs
@@ -19,16 +19,6 @@ namespace Mariani_GestionePrezzi
         public FormVisualizzatoreMenu()
         {
             InitializeComponent();
-
-            // Aggiunge le colonne "Nome" e "Prezzo" alla DataGridView
-            dataGridViewProdotti.Columns.Add("Nome", "Nome");
-            dataGridViewProdotti.Columns.Add("Prezzo", "Prezzo");
-
-            // Imposta la modalità di ridimensionamento automatico delle colonne in base al contenuto
-            foreach (DataGridViewColumn colonna in dataGridViewProdotti.Columns)
-            {
-                colonna.AutoSizeMode = DataGridViewAutoSizeColumnMode.AllCells;
-            }
         }
 
         // Evento che si verifica quando la form viene caricata
@@ -40,20 +30,94 @@ namespace Mariani_GestionePrezzi
         // Metodo per caricare i prodotti con il loro prezzo finale dal file JSON
         private void CaricaProdottiConPrezzoFinale()
         {
-            string filePath = "prodottiConPrezzoFinale.json";  // Percorso del file JSON
+            string filePath = "prodottiConPrezzoFinale.json";
             if (File.Exists(filePath))
             {
-                string json = File.ReadAllText(filePath);  // Legge il contenuto del file JSON
-                List<ProdottoConPrezzoFinale> prodottiConPrezzoFinale = JsonConvert.DeserializeObject<List<ProdottoConPrezzoFinale>>(json);  // Deserializza il contenuto in una lista di ProdottoConPrezzoFinale
+                string json = File.ReadAllText(filePath);
+                List<ProdottoConPrezzoFinale> prodottiConPrezzoFinale = JsonConvert.DeserializeObject<List<ProdottoConPrezzoFinale>>(json);
 
-                dataGridViewProdotti.Rows.Clear();  // Cancella tutte le righe esistenti nella DataGridView
+                GeneraCSS();  // Genera il file CSS per la formattazione della pagina HTML
+
+                StringBuilder html = new StringBuilder();
+
+                html.AppendLine("<!DOCTYPE html>");
+                html.AppendLine("<html>");
+                html.AppendLine("<head>");
+                html.AppendLine("<link rel=\"stylesheet\" type=\"text/css\" href=\"style.css\">");
+                html.AppendLine("</head>");
+                html.AppendLine("<body>");
+                html.AppendLine("<h2>Menu</h2>");
+                html.AppendLine("<table>");
+                html.AppendLine("<tr><th>Nome</th><th>Prezzo</th></tr>");
+
                 foreach (var prodotto in prodottiConPrezzoFinale)
                 {
-                    // Aggiunge una nuova riga alla DataGridView con il nome e il prezzo finale del prodotto
-                    dataGridViewProdotti.Rows.Add(prodotto.Nome, prodotto.PrezzoFinale);
+                    html.AppendLine($"<tr><td>{prodotto.Nome}</td><td>{prodotto.PrezzoFinale}</td></tr>");
                 }
 
-                dataGridViewProdotti.AutoResizeColumns();  // Ridimensiona automaticamente le colonne in base al contenuto
+                html.AppendLine("</table>");
+                html.AppendLine("</body>");
+                html.AppendLine("</html>");
+
+                File.WriteAllText("menu.html", html.ToString());
+            }
+        }
+
+        private void GeneraCSS()
+        {
+            // Genera il file CSS
+            string css = @"
+            body {
+                font-family: Arial, sans-serif;
+                background-color: #f0f0f0;
+                padding: 20px;
+            }
+
+            h2 {
+                color: #333;
+                font-size: 2em;
+                text-align: center;
+            }
+
+            table {
+                width: 100%;
+                border-collapse: collapse;
+                margin-top: 20px;
+            }
+
+            th, td {
+                border: 1px solid #999;
+                padding: 10px;
+                text-align: left;
+            }
+
+            th {
+                background-color: #333;
+                color: #fff;
+            }
+
+            td {
+                background-color: #fff;
+                color: #333;
+            }
+
+            tr:nth-child(even) td {
+                background-color: #f2f2f2;
+            }";
+
+            File.WriteAllText("style.css", css);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string filePath = "menu.html";
+            if (File.Exists(filePath))
+            {
+                System.Diagnostics.Process.Start(filePath);
+            }
+            else
+            {
+                MessageBox.Show("Il file menu.html non esiste.");
             }
         }
     }
